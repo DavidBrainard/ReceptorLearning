7c987b25b073a0ad9010f98c50eb2a928cff33d7
Noah Benson <n@mango.local>
2013-11-06 19:16:45 -0500

    Added a short-test

--- src/main/clojure/brainardlab/nben/retina/util.clj
+++ src/main/clojure/brainardlab/nben/retina/util.clj
@@ -44,7 +44,16 @@
                  :sizes [20]
                  :S-cone-flags [:human]
                  :samples [2500000]
-                 :runs [3]}})
+                 :runs [3]}
+      :test-short {:surrounds [[0.25 3.0] nil]
+                   :L-to-Ms [[16 1] [8 1] [4 1]]
+                   :M-lambda-maxs [535 540]
+                   :L-lambda-maxs [558.9]
+                   :S-lambda-maxs [420.7]
+                   :sizes [20]
+                   :S-cone-flags [:human]
+                   :samples [1250000]
+                   :runs [4]}})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
