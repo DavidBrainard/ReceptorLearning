bf30f3dabf4b5de588edf2f0ebeee710b312c85c
Noah C. Benson <n@nben.net>
2014-01-09 10:10:25 -0500

    Changed the doc directory to have an html subdirectory.

--- project.clj
+++ project.clj
@@ -4,14 +4,18 @@
   :url "http://github.com/nben/retina"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ;; We want to be able to use large amounts of memory
+  ;; This is something that you'll likely want to set for each machine on which this library is used
+  :jvm-opts ["-Xmx7G"]
   ;; dependencies
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [net.sourceforge.jmatio/jmatio "1.0"]
                  [incanter "1.5.4"]]
+  :plugins [[codox "0.6.6"]]
+  ;; for generating codox documentation
+  :codox {:output-dir "doc/html"}
   :resource-paths ["resources/mdsj.jar"]
-  ;; we want to be able to use large amounts of memory
-  :jvm-opts ["-Xmx7G"]
   ;; location of source codes
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
